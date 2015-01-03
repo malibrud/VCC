aacfdc38cfcff410b695c6b6c125a78eca323ad6
David Downs <builderofrobots@yahoo.com>
2015-01-03 11:47:38 -0500

    floor images and line finding

---
+++ lineFinding.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+
+from SimpleCV import *
+
+while 1:
+    img = Image("floorImages/dottedLine.jpg")
+    whiteLine = img.colorDistance(Color.WHITE)
+    line = img - whiteLine
+    blobArray = line.findBlobs()
+    #print blobs[:].meanColor()
+    for blob in blobArray:
+        if blob.area() > 200:
+            blob.draw(color=Color.RED, width=-1)
+            line.drawCircle((blob.centroid()),10,color=Color.BLUE)
+    line.show()
