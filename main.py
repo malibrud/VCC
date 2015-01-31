3c7ddcde224f9341bc761f0b8cf82ae72c1cc249
David Downs <builderofrobots@yahoo.com>
2015-01-31 11:53:18 -0500

    robot to world

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python2.7
 
 from SimpleCV import *
-#from motorControl import *
+from motorControl import *
 from coordConvert import *
 from imageProcessing import *
 import time
 
-#mc = MotorControl()
+mc = MotorControl()
 cc = CoordConvert()
 ip = ImageProcessing()
 cam = Camera(1)
@@ -20,8 +20,6 @@ while 1:
     for i in range(0, len(lineSegments)):
         s = lineSegments[i]
         r = cc.rasterToRobot(s[0], s[1])
-        print r[0][0]
-        print
         w = cc.robotToWorld(0,0, r.item((0,0)), r.item((1,0)), 0)
         print w
     #print lineSegments
