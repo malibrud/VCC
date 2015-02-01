9cbeffa4175ea2ebb795ba26b5d1c765fe8892c0
malibrud <malibrud@gmail.com>
2015-01-31 23:58:30 -0500

    added comments for slam

--- main.py
+++ main.py
@@ -14,9 +14,23 @@ cam = Camera(1)
 robotX = 0
 robotY = 0
 
+
+# Initialize landmark list to empty
+# Initialize global location to 0,0,0
 while 1:
+    # Get an image
     img = cam.getImage()
+
+    # find blobs
     lineSegments = ip.lineFinding(img)
+
+    # for each located blob, convert the position from camera to robot
+    # and them from robot to world coordinate system
+    # We may need to filter them based on which ones are fully in the view 
+    # (i.e. exclude those which are on the edge).
+    # We may also want to find other features such as corners or edges.
+    # For each paper, there will be 4 corners.  Also corners can be used 
+    # if a partial paper or tape is in view.
     for i in range(0, len(lineSegments)):
         s = lineSegments[i]
         r = cc.rasterToRobot(s[0], s[1])
@@ -25,3 +39,21 @@ while 1:
         w = cc.robotToWorld(0,0, r.item((0,0)), r.item((1,0)), 0)
         print w
     #print lineSegments
+
+    # We will need to compare each blob in the world or robot coordinate system
+    # to those on the known landmark list.
+    # If an object is on the known landmark list.  Do a match.
+    # if an object is not on the known landmark list, add it to the list.
+
+    # Based on our known land marks and the model output estimate our new robot
+    # position.  This will be moderately complex.
+
+    # Determine next position to drive toward.
+
+    # Drive the robot
+
+    # Sleep ??
+
+    # Update current time.
+
+    # run model to estimate current position
