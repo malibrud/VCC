e1118690f56ca6bb3bd6ac173e94e9645e705ba2
David Downs <builderofrobots@gmail.com>
2015-04-25 13:38:17 -0400

    new webcam picture

--- imageTestDave.py
+++ imageTestDave.py
@@ -1,21 +1,19 @@
 from SimpleCV import *
 import time
 
-colorArray = [(85,118,179), (147,126,100), (99,160,127),(209,158,129),(193,157,171),(127,131,177),(181, 196, 104)]  
+#colorArray = [(85,118,179), (147,126,100), (99,160,127),(209,158,129),(193,157,171),(127,131,177),(181, 196, 104)]  
 blobCenterArray = []
 while 1:
+	img = Image("image.bmp")
+	colorSelect = img.colorDistance((193,216,224)).binarize(40)
+	blobArray = colorSelect.findBlobs()
+	for blob in blobArray:
+		if blob.mHoleContour < 2:
+			if blob.area() > 1000:
+				x = blob.centroid()[0]
+				y = blob.centroid()[1]
+			 	img.drawText("line",x, y, color=Color.RED, fontsize=28)	
 
-	img = Image("image8.bmp")
-	for color in colorArray:
-		colorSelect = img.colorDistance(color).binarize(40)
-		blobArray = colorSelect.findBlobs()
-		for blob in blobArray:
-			if blob.mHoleContour < 2:
-				if blob.area() > 10000:
- 					blob.draw(color=Color.GREEN, width=-1)
-					blobCenterArray.append(blob.centroid())			
-					colorSelect.show()
-	
-	print blobCenterArray
+	img.show()
 	time.sleep(2)
 
